489f23dee95efc3930e66ab2840ad09b95c5e9d6
Abhilash <abhilash.ghare@gmail.com>
2021-02-24 12:19:02 +0530

    aws

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello ABhilash 1st AWS demo!'));
+app.get('/', (req, res) => res.send('Hello ABhilash 1st AWS demo changes done!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
